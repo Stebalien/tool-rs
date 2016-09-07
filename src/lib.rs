0eba2779e2a8a111102645377eedccfb712c27f0
Steven Allen <steven@stebalien.com>
2016-09-07 19:17:08 -0400

    Add test

--- src/lib.rs
+++ src/lib.rs
@@ -26,3 +26,13 @@ pub fn second<P: seq::Pair>(seq: P) -> P::Second {
 pub fn third<P: seq::Triple>(seq: P) -> P::Third {
     seq.third()
 }
+
+#[test]
+fn test_seq() {
+    assert_eq!(first((1, 2)), 1);
+    assert_eq!(first(&(1, 2)), &1);
+    assert_eq!(second((1, 2)), 2);
+    assert_eq!(third(&[] as &[i32]), None);
+    assert_eq!(second(&[1,2][..]), Some(&2));
+    assert_eq!(second(&[1,2]), &2);
+}
