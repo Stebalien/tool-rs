9abfe5c4227112912ce230e1a8ebc633b6d33826
Steven Allen <steven@stebalien.com>
2016-09-07 19:46:12 -0400

    Use macros provided by bluss

--- src/seq.rs
+++ src/seq.rs
@@ -13,231 +13,143 @@ pub trait Triple: Pair {
     fn third(self) -> Self::Third;
 }
 
-// Normal
-
-impl<A> Singleton for (A,) {
-    type First = A;
-    fn first(self) -> Self::First {
-        self.0
+macro_rules! one {
+    ($($t:tt)*) => {1}
+}
+macro_rules! count {
+    ($($t:ident),*) => {
+        $(one!($t) +)* 0
     }
 }
 
-impl<A, B> Singleton for (A, B) {
-    type First = A;
-    fn first(self) -> Self::First {
-        self.0
+macro_rules! call {
+    ($mac:ident) => {
+        $mac!{A, B, C, D, E, F, G, H, I, J,}
     }
 }
 
-impl<A, B, C> Singleton for (A, B, C) {
-    type First = A;
-    fn first(self) -> Self::First {
-        self.0
+macro_rules! impl_first {
+    () => {};
+    ($F:ident, $($A:ident,)*) => {
+        impl_first!{$($A,)*}
+        impl<$F, $($A),*> Singleton for ($F, $($A),*) {
+            type First = $F;
+            fn first(self) -> Self::First {
+                self.0
+            }
+        }
+
+        impl<'a, $F, $($A),*> Singleton for &'a ($F, $($A),*) {
+            type First = &'a $F;
+            fn first(self) -> Self::First {
+                &self.0
+            }
+        }
+
+        impl<'a, $F, $($A),*> Singleton for &'a mut ($F, $($A),*) {
+            type First = &'a mut $F;
+            fn first(self) -> Self::First {
+                &mut self.0
+            }
+        }
+        impl<'a, $F> Singleton for &'a [$F; count!($F $(,$A)*)] {
+            type First = &'a $F;
+            fn first(self) -> Self::First {
+                &self[0]
+            }
+        }
+
+        impl<'a, $F> Singleton for &'a mut [$F; count!($F $(,$A)*)] {
+            type First = &'a mut $F;
+            fn first(self) -> Self::First {
+                &mut self[0]
+            }
+        }
     }
 }
 
-impl<A, B> Pair for (A, B) {
-    type Second = B;
-    fn second(self) -> Self::Second {
-        self.1
+macro_rules! impl_second {
+    ($F:ident,) => {};
+    ($F:ident, $S:ident, $($A:ident,)*) => {
+        impl_second!{$S, $($A,)*}
+        impl<$F, $S, $($A),*> Pair for ($F, $S, $($A),*) {
+            type Second = $S;
+            fn second(self) -> Self::Second {
+                self.1
+            }
+        }
+
+        impl<'a, $F, $S, $($A),*> Pair for &'a ($F, $S, $($A),*) {
+            type Second = &'a $S;
+            fn second(self) -> Self::Second {
+                &self.1
+            }
+        }
+
+        impl<'a, $F, $S, $($A),*> Pair for &'a mut ($F, $S, $($A),*) {
+            type Second = &'a mut $S;
+            fn second(self) -> Self::Second {
+                &mut self.1
+            }
+        }
+        impl<'a, $S> Pair for &'a [$S; count!($F, $S $(,$A)*)] {
+            type Second = &'a $S;
+            fn second(self) -> Self::Second {
+                &self[1]
+            }
+        }
+
+        impl<'a, $S> Pair for &'a mut [$S; count!($F, $S $(,$A)*)] {
+            type Second = &'a mut $S;
+            fn second(self) -> Self::Second {
+                &mut self[1]
+            }
+        }
     }
 }
 
+macro_rules! impl_third {
+    ($F:ident, $S:ident,) => {};
+    ($F:ident, $S:ident, $T:ident, $($A:ident,)*) => {
+        impl_third!{$S, $T, $($A,)*}
+        impl<$F, $S, $T, $($A),*> Triple for ($F, $S, $T, $($A),*) {
+            type Third = $T;
+            fn third(self) -> Self::Third {
+                self.2
+            }
+        }
 
-impl<A, B, C> Pair for (A, B, C) {
-    type Second = B;
-    fn second(self) -> Self::Second {
-        self.1
+        impl<'a, $F, $S, $T, $($A),*> Triple for &'a ($F, $S, $T, $($A),*) {
+            type Third = &'a $T;
+            fn third(self) -> Self::Third {
+                &self.2
+            }
+        }
+
+        impl<'a, $F, $S, $T, $($A),*> Triple for &'a mut ($F, $S, $T, $($A),*) {
+            type Third = &'a mut $T;
+            fn third(self) -> Self::Third {
+                &mut self.2
+            }
+        }
+        impl<'a, $T> Triple for &'a [$T; count!($F, $S, $T $(,$A)*)] {
+            type Third = &'a $T;
+            fn third(self) -> Self::Third {
+                &self[2]
+            }
+        }
+
+        impl<'a, $T> Triple for &'a mut [$T; count!($F, $S, $T $(,$A)*)] {
+            type Third = &'a mut $T;
+            fn third(self) -> Self::Third {
+                &mut self[2]
+            }
+        }
     }
 }
-
-impl<A, B, C> Triple for (A, B, C) {
-    type Third = C;
-    fn third(self) -> Self::Third {
-        self.2
-    }
-}
-
-
-// By Ref
-
-impl<'a, A> Singleton for &'a (A,) {
-    type First = &'a A;
-    fn first(self) -> Self::First {
-        &self.0
-    }
-}
-
-impl<'a, A, B> Singleton for &'a (A, B) {
-    type First = &'a A;
-    fn first(self) -> Self::First {
-        &self.0
-    }
-}
-
-impl<'a, A, B, C> Singleton for &'a (A, B, C) {
-    type First = &'a A;
-    fn first(self) -> Self::First {
-        &self.0
-    }
-}
-
-impl<'a, A, B> Pair for &'a (A, B) {
-    type Second = &'a B;
-    fn second(self) -> Self::Second {
-        &self.1
-    }
-}
-
-impl<'a, A, B, C> Pair for &'a (A, B, C) {
-    type Second = &'a B;
-    fn second(self) -> Self::Second {
-        &self.1
-    }
-}
-
-impl<'a, A, B, C> Triple for &'a (A, B, C) {
-    type Third = &'a C;
-    fn third(self) -> Self::Third {
-        &self.2
-    }
-}
-
-// Tuple by mut ref
-
-impl<'a, A> Singleton for &'a mut (A,) {
-    type First = &'a mut A;
-    fn first(self) -> Self::First {
-        &mut self.0
-    }
-}
-
-impl<'a, A, B> Singleton for &'a mut (A, B) {
-    type First = &'a A;
-    fn first(self) -> Self::First {
-        &mut self.0
-    }
-}
-
-impl<'a, A, B, C> Singleton for &'a mut (A, B, C) {
-    type First = &'a mut A;
-    fn first(self) -> Self::First {
-        &mut self.0
-    }
-}
-
-impl<'a, A, B> Pair for &'a mut (A, B) {
-    type Second = &'a mut B;
-    fn second(self) -> Self::Second {
-        &mut self.1
-    }
-}
-
-impl<'a, A, B, C> Pair for &'a mut (A, B, C) {
-    type Second = &'a B;
-    fn second(self) -> Self::Second {
-        &mut self.1
-    }
-}
-
-impl<'a, A, B, C> Triple for &'a mut (A, B, C) {
-    type Third = &'a mut C;
-    fn third(self) -> Self::Third {
-        &mut self.2
-    }
-}
-
-// Array
-
-// TODO (drop bleh...)
-
-// Array Ref
-
-impl<'a, A> Singleton for &'a [A; 1] {
-    type First = &'a A;
-    fn first(self) -> Self::First {
-        &self[0]
-    }
-}
-
-impl<'a, A> Singleton for &'a [A; 2] {
-    type First = &'a A;
-    fn first(self) -> Self::First {
-        &self[0]
-    }
-}
-
-impl<'a, A> Singleton for &'a [A; 3] {
-    type First = &'a A;
-    fn first(self) -> Self::First {
-        &self[0]
-    }
-}
-
-impl<'a, A> Pair for &'a [A; 2] {
-    type Second = &'a A;
-    fn second(self) -> Self::Second {
-        &self[1]
-    }
-}
-
-impl<'a, A> Pair for &'a [A; 3] {
-    type Second = &'a A;
-    fn second(self) -> Self::Second {
-        &self[1]
-    }
-}
-
-impl<'a, A> Triple for &'a [A; 3] {
-    type Third = &'a A;
-    fn third(self) -> Self::Third {
-        &self[2]
-    }
-}
-
-// Array by mut ref
-
-impl<'a, A> Singleton for &'a mut [A; 1] {
-    type First = &'a mut A;
-    fn first(self) -> Self::First {
-        &mut self[0]
-    }
-}
-
-impl<'a, A> Singleton for &'a mut [A; 2] {
-    type First = &'a mut A;
-    fn first(self) -> Self::First {
-        &mut self[0]
-    }
-}
-
-impl<'a, A> Singleton for &'a mut [A; 3] {
-    type First = &'a mut A;
-    fn first(self) -> Self::First {
-        &mut self[0]
-    }
-}
-
-impl<'a, A> Pair for &'a mut [A; 2] {
-    type Second = &'a mut A;
-    fn second(self) -> Self::Second {
-        &mut self[1]
-    }
-}
-
-impl<'a, A> Pair for &'a mut [A; 3] {
-    type Second = &'a mut A;
-    fn second(self) -> Self::Second {
-        &mut self[1]
-    }
-}
-
-impl<'a, A> Triple for &'a mut [A; 3] {
-    type Third = &'a mut A;
-    fn third(self) -> Self::Third {
-        &mut self[2]
-    }
-}
+call!(impl_first);
+call!(impl_second);
+call!(impl_third);
 
 // Slice
 
